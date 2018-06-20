dda59c4533310b8db44dfdabce8ccb3998650e20
Jan Jelschen <jan.jelschen@itemis.de>
2018-06-20 17:30:35 +0200

    expand/collapse, copy to clipboard

--- src/app/modules/test-step-selector/test-step-selector.component.spec.ts
+++ src/app/modules/test-step-selector/test-step-selector.component.spec.ts
@@ -5,7 +5,8 @@ import { DefaultTestStepService, TestStepNode, TestStepNodeType, TestStepService
 import { mock, when, instance } from 'ts-mockito';
 import { By } from '@angular/platform-browser';
 import { TreeViewerModule } from '@testeditor/testeditor-commons';
-import { element } from 'protractor';
+import { MessagingService, MessagingModule } from '@testeditor/messaging-service';
+import { Clipboard } from 'ts-clipboard';
 
 describe('TestStepSelectorComponent', () => {
 
@@ -38,13 +39,14 @@ describe('TestStepSelectorComponent', () => {
   let component: TestStepSelectorComponent;
   let fixture: ComponentFixture<TestStepSelectorComponent>;
   let testStepServiceMock: TestStepService;
+  let messagingService: MessagingService;
 
   beforeEach(async(() => {
     testStepServiceMock = mock(DefaultTestStepService);
     when(testStepServiceMock.getTestSteps()).thenResolve(testStepTree);
 
     TestBed.configureTestingModule({
-      imports: [ TreeViewerModule ],
+      imports: [ TreeViewerModule, MessagingModule.forRoot() ],
       declarations: [ TestStepSelectorComponent ],
       providers: [{provide: TestStepService, useValue: instance(testStepServiceMock)}]
     })
@@ -52,6 +54,7 @@ describe('TestStepSelectorComponent', () => {
   }));
 
   beforeEach(() => {
+    messagingService = TestBed.get(MessagingService);
     fixture = TestBed.createComponent(TestStepSelectorComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -62,16 +65,86 @@ describe('TestStepSelectorComponent', () => {
   });
 
   it('displays test steps retrieved from service', fakeAsync(() => {
-    // given
-
-
     // when
     component.updateModel();
     tick();
+    fixture.detectChanges();
 
     // then
     const elementNames = testStepTree2Array(testStepTree).map((node) => node.displayName);
     expect(fixture.debugElement.queryAll(By.css('.tree-view-element'))
       .map((elem) => elem.nativeElement.innerText)).toEqual(elementNames);
   }));
+
+  it('collapses node on click', fakeAsync(() => {
+    // given
+    component.updateModel();
+    tick();
+    fixture.detectChanges();
+    const rootElement = fixture.debugElement.query(By.css('.tree-view-element')).nativeElement;
+
+    // when
+    rootElement.click();
+    tick();
+    fixture.detectChanges();
+
+    // then
+    expect(fixture.debugElement.queryAll(By.css('.tree-view-element'))
+      .map((elem) => elem.nativeElement.innerText)).toEqual([ 'root' ]);
+  }));
+
+  it('copies test step to clipboard when double-clicking leaf element', fakeAsync(() => {
+    // given
+    component.updateModel();
+    tick();
+    fixture.detectChanges();
+    const treeElements = fixture.debugElement.queryAll(By.css('.tree-view-item-key'));
+
+    // when
+    treeElements[3].triggerEventHandler('dblclick', new MouseEvent('dblclick'));
+    fixture.detectChanges();
+    tick();
+
+    // then
+    expect(Clipboard._data).toEqual('some interaction');
+  }));
+
+  it('does not copy anything to clipboard when double-clicking inner (non-leaf) element', fakeAsync(() => {
+    // given
+    Clipboard._data = '';
+    component.updateModel();
+    tick();
+    fixture.detectChanges();
+    const treeElements = fixture.debugElement.queryAll(By.css('.tree-view-item-key'));
+
+    // when
+    treeElements[2].triggerEventHandler('dblclick', new MouseEvent('dblclick'));
+    fixture.detectChanges();
+    tick();
+
+    // then
+    expect(Clipboard._data).toEqual('');
+  }));
+
+  it('sends "copied to clipboard!" message to snackbar via message bus on double-click', fakeAsync(() => {
+    // given
+    let actualMessage = null;
+    const subscription = messagingService.subscribe('snackbar.display.notification', (payload) => actualMessage = payload.message);
+    component.updateModel();
+    tick();
+    fixture.detectChanges();
+    const treeElements = fixture.debugElement.queryAll(By.css('.tree-view-item-key'));
+
+    // when
+    treeElements[3].triggerEventHandler('dblclick', new MouseEvent('dblclick'));
+    fixture.detectChanges();
+    tick();
+
+    // then
+    expect(actualMessage).toEqual('copied to clipboard!');
+
+    // cleanup
+    subscription.unsubscribe();
+  }));
+
 });
